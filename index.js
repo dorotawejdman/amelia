98eea4b011480754b04c7a0192a001ee2f8074fd
dorotawejdman <dorota.wejdman@gmail.com>
2022-02-26 13:10:39 +0100

    Add additional form fields, and form js logic

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ document.addEventListener("scroll", function (e) {
     nav[0].classList.remove("nav-transparent");
   }
 });
+
+// Form change listener
+window.addEventListener("load", function () {
+  document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let email = e.target.elements.email.value;
+    let message = e.target.elements.message.value;
+    let departureDate = e.target.elements.departureDate.value;
+    let arrivalDate = e.target.elements.arrivalDate.value;
+    let adultsNumber = e.target.elements.adultsNumber.value;
+    let kidsNumber = e.target.elements.kidsNumber.value;
+    let animalsNumber = e.target.elements.animalsNumber.value;
+    let emailContent = `Wiadomość od ${email}\nTreść:\n${message}\n`;
+    let additionalContent = `Data przyjazdu:  ${arrivalDate}\nData wyjazdu:  ${departureDate}\nLiczba dorosłych:  ${adultsNumber}\nLiczba dzieci:  ${kidsNumber}\nLiczba zwierząt:  ${animalsNumber}`;
+    console.log(emailContent);
+    if (
+      arrivalDate ||
+      departureDate ||
+      adultsNumber ||
+      kidsNumber ||
+      animalsNumber
+    ) {
+      console.log(emailContent + additionalContent);
+    }
+  });
+});
