8d0932206b5b3b3e0ed31979a87df66482693a98
dorotawejdman <dorota.wejdman@gmail.com>
2022-02-27 14:47:10 +0100

    Seperate scripts

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-// Dynamic nav classes change on scroll
-nav = document.getElementsByTagName("nav");
-document.addEventListener("scroll", function (e) {
-  var y = window.scrollY;
-  var height =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.body.clientHeight;
-  if (y > 0.85 * height) {
-    nav[0].classList.add("nav-solid");
-  }
-  if (y < 0.85 * height) {
-    nav[0].classList.remove("nav-solid");
-  }
-});
+let activeSubpages = {
+  contact: false,
+  rooms: false,
+  main: true,
+};
 
-// Form change listener
-window.addEventListener("load", function () {
-  document.getElementById("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    let email = e.target.elements.email.value;
-    let message = e.target.elements.message.value;
-    let departureDate = e.target.elements.departureDate.value;
-    let arrivalDate = e.target.elements.arrivalDate.value;
-    let adultsNumber = e.target.elements.adultsNumber.value;
-    let kidsNumber = e.target.elements.kidsNumber.value;
-    let animalsNumber = e.target.elements.animalsNumber.value;
-    let emailContent = `Wiadomość od ${email}\nTreść:\n${message}\n`;
-    let additionalContent = `Data przyjazdu:  ${arrivalDate}\nData wyjazdu:  ${departureDate}\nLiczba dorosłych:  ${adultsNumber}\nLiczba dzieci:  ${kidsNumber}\nLiczba zwierząt:  ${animalsNumber}`;
-    console.log(emailContent);
-    if (
-      arrivalDate ||
-      departureDate ||
-      adultsNumber ||
-      kidsNumber ||
-      animalsNumber
-    ) {
-      console.log(emailContent + additionalContent);
-    }
-  });
-});
+// Navigation indicator change
+let navLinks = document.getElementsByClassName("nav-menu__link");
+function subpageSelected(subpage) {
+  Array.prototype.forEach.call(navLinks, (link) =>
+    link.classList.remove("active")
+  );
+  navLinks[subpage].classList.add("active");
+  console.log(activeSubpages);
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+// Dynamic nav classes change on scroll
+nav = document.getElementsByTagName("nav");
+document.addEventListener("scroll", function (e) {
+  var y = window.scrollY;
+  var height =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+  if (y > 0.85 * height) {
+    nav[0].classList.add("nav-solid");
+  }
+  if (y < 0.85 * height) {
+    nav[0].classList.remove("nav-solid");
+  }
+});
+
+// Scroll on button click to specific sections
+// function scrollToContact
+// var elementToView = document.getElementById("sectionId");
+// elementToView.scrollIntoView();
+
+// Form change listener
+window.addEventListener("load", function () {
+  document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let email = e.target.elements.email.value;
+    let message = e.target.elements.message.value;
+    let departureDate = e.target.elements.departureDate.value;
+    let arrivalDate = e.target.elements.arrivalDate.value;
+    let adultsNumber = e.target.elements.adultsNumber.value;
+    let kidsNumber = e.target.elements.kidsNumber.value;
+    let animalsNumber = e.target.elements.animalsNumber.value;
+    let emailContent = `Wiadomość od ${email}\nTreść:\n${message}\n`;
+    let additionalContent = `Data przyjazdu:  ${arrivalDate}\nData wyjazdu:  ${departureDate}\nLiczba dorosłych:  ${adultsNumber}\nLiczba dzieci:  ${kidsNumber}\nLiczba zwierząt:  ${animalsNumber}`;
+    console.log(emailContent);
+    if (
+      arrivalDate ||
+      departureDate ||
+      adultsNumber ||
+      kidsNumber ||
+      animalsNumber
+    ) {
+      console.log(emailContent + additionalContent);
+    }
+  });
+});
