839c2cd1444b60a4d31f7cd12db412426cfdfa42
Dorota Wejdman <dorota.wejdman@futurum.tech>
2022-02-28 12:37:56 +0100

    Create script for changing nav indicator on scroll and page switching

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-let activeSubpages = {
-  contact: false,
-  rooms: false,
-  main: true,
-};
-
 // Navigation indicator change
 let navLinks = document.getElementsByClassName("nav-menu__link");
+
 function subpageSelected(subpage) {
-  Array.prototype.forEach.call(navLinks, (link) =>
-    link.classList.remove("active")
-  );
-  navLinks[subpage].classList.add("active");
-  console.log(activeSubpages);
+  window.sessionStorage.setItem('activeLink',subpage);
 }
+
+function setActiveLink(linkId) {
+  Array.prototype.forEach.call(navLinks, (link) =>
+  link.classList.remove("active"));
+  navLinks[linkId].classList.add("active");
+}
+window.onload = function() {
+  let activeLink = window.sessionStorage.getItem('activeLink')
+  setActiveLink(activeLink)
+}
+
+window.addEventListener("scroll", function(event) {
+      let contactTopDistance = this.document.getElementById('contact-section').getBoundingClientRect().top
+      let contactBottom = this.document.getElementById('contact-section').getBoundingClientRect().bottom
+      let roomsTopDistance = this.document.getElementById('rooms-section').getBoundingClientRect().top
+    if(contactTopDistance<500) setActiveLink(4)
+    else if(roomsTopDistance<500) setActiveLink(1)
+    else setActiveLink(0)
+}, false);
