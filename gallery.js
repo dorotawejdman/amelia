e5c31250da7e21b596e05c576c2a5f87e24af60b
Dorota Wejdman <dorota.wejdman@futurum.tech>
2022-03-02 10:49:23 +0100

    Create seperate galleries for different category

--- gallery.js
+++ gallery.js
@@ -1,18 +1,22 @@
 let imagesPerFolder = {
-  Budynek: 6,
-  Widoki: 4,
-  Otoczenie: 6,
+  Budynek: 5,
+  Widoki: 0,
+  Otoczenie: 2,
 };
 
-Object.keys(imagesPerFolder).forEach((folder) => {
+Object.keys(imagesPerFolder).forEach((folder, idKey) => {
   console.log("." + folder);
   let containerElement = document.querySelector("." + folder);
   for (var i = 1; i <= imagesPerFolder[folder]; i++) {
     var src = "./Assets/Images/Galeria/" + folder + "/" + (i + 1) + ".jpg";
+    let link=document.createElement('a');
+    link.setAttribute('href', src)
+    link.classList.add('lightbox'+idKey)
+    link.classList.add('d-none')
     var img = new Image();
     img.src = src;
     img.height = 500;
-
-    containerElement.appendChild(img);
+    link.appendChild(img)
+    containerElement.appendChild(link);
   }
 });
