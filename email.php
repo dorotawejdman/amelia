0e0cfd938eb934398eb13624781e310df213f402
dorotawejdman <dorota.wejdman@gmail.com>
2022-03-19 11:35:20 +0100

    Modify php script

--- email.php
+++ email.php
@@ -2,27 +2,33 @@
     
    $message_send = false;
 
-    $q ="";
-    echo $q;
-
     if(isset($_POST['email']) && $_POST['email']!= ""){
 
         if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
         {
 
-            // $name = $_POST['name'];
             $email = $_POST['email'];
             $message = $_POST['message'];
+            $adultsNumber = $_POST['adultsNumber'];
+            $kidsNumber = $_POST['kidsNumber'];
+            $animalsNumber = $_POST['animalsNumber'];
+            $arrivalDate = $_POST['arrivalDate'];
+            $departureDate = $_POST['departureDate'];
             
+
             $myMail = "dorota.wejdman@gmail.com";
-            $subject = "MAIL: T.devWeb od:".$name;
+            $subject = "MAIL od: ".$email;
             $body = ""; 
             
-            // $body .= "Wiadomość od: ".$name."\r\n";
-            $body .= "e-mail: ".$email."\r\n";
+            $body .= "E-mail: ".$email."\r\n";
             $body .= "Treść wiadomości: ".$message."\r\n";
+            $body .= "Szczegółowe informacje\n";
+            $body .= "Termin: ".$arrivalDate." - ".$departureDate."\r\n";
+            $body .= "Liczba osób dorosłych: ".$adultsNumber."\r\n";
+            $body .= "Liczba dzieci: ".$kidsNumber."\r\n";
+            $body .= "Liczba zwierząt: ".$animalsNumber."\r\n";
             
-           // mail($myMail,$subject,$body);
+           mail($myMail,$subject,$body);
 
             $message_send = true;
             echo $message_send;
